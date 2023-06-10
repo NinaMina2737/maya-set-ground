6f528d930182eb186cf5fda9fdaf121ee339aa6b
NinaMina2737 <87418579+NinaMina2737@users.noreply.github.com>
2023-06-11 04:05:02 +0900

    Update quaternion.py

--- src/matrix.py
+++ src/matrix.py
@@ -3,21 +3,31 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import math
-from .vector import Vector
+from vector import Vector3
 
 class Matrix:
-    def __init__(self, rows=4, cols=4, data=None, translation=None, rotation=None, scale=None):
-        if data is None:
+    def __init__(self, rows=4, cols=4, data=None):
+        if not rows is None and not cols is None:
+            if not isinstance(rows, int) or not isinstance(cols, int):
+                raise ValueError("Matrix.__init__ takes two integers as rows and columns")
+            if rows < 1 or cols < 1:
+                raise ValueError("Matrix.__init__ takes two positive integers as rows and columns")
+            if not data is None:
+                raise ValueError("Matrix.__init__ takes either rows and columns or data")
             self.rows = rows
             self.cols = cols
             self.data = [[0 for j in range(cols)] for i in range(rows)]
-        else:
+        if not data is None:
             if not isinstance(data, list):
                 raise ValueError("Matrix.__init__ takes a list of lists")
             if any(not isinstance(row, list) for row in data):
                 raise ValueError("Matrix.__init__ takes a list of lists")
             if any(not isinstance(item, (int, float)) for row in data for item in row):
                 raise ValueError("Matrix.__init__ takes a list of lists of numbers")
+            if any(len(row) != len(data[0]) for row in data):
+                raise ValueError("Matrix.__init__ takes a list of lists of the same length")
+            if not rows is None or not cols is None:
+                raise ValueError("Matrix.__init__ takes either rows and columns or data")
             self.rows = len(data)
             self.cols = len(data[0])
             self.data = data
@@ -39,8 +49,8 @@ class Matrix:
             raise ValueError("Matrix.__mul__ takes a matrix with the same number of rows as the number of columns of this matrix")
         if isinstance(other, Matrix):
             return Matrix([[sum([self.data[i][k] * other.data[k][j] for k in range(self.cols)]) for j in range(other.cols)] for i in range(self.rows)])
-        elif isinstance(other, Vector):
-            return Vector([sum([self.data[i][j] * other.data[j] for j in range(self.cols)]) for i in range(self.rows)])
+        elif isinstance(other, Vector3):
+            return Vector3([sum([self.data[i][j] * other.data[j] for j in range(self.cols)]) for i in range(self.rows)])
         else:
             return Matrix([[self.data[i][j] * other for j in range(self.cols)] for i in range(self.rows)])
 
@@ -49,7 +59,7 @@ class Matrix:
             raise ValueError("Matrix.__rmul__ takes a matrix with the same number of columns as the number of rows of this matrix")
         if isinstance(other, Matrix):
             return Matrix([[sum([other.data[i][k] * self.data[k][j] for k in range(self.cols)]) for j in range(other.cols)] for i in range(self.rows)])
-        elif isinstance(other, Vector):
-            return Vector([sum([other.data[j] * self.data[i][j] for j in range(self.cols)]) for i in range(self.rows)])
+        elif isinstance(other, Vector3):
+            return Vector3([sum([other.data[j] * self.data[i][j] for j in range(self.cols)]) for i in range(self.rows)])
         else:
             return Matrix([[other * self.data[i][j] for j in range(self.cols)] for i in range(self.rows)])
