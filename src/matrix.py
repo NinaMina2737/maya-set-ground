a3354a0df098e1e7e9af333ce46c4390114b5e37
NinaMina2737 <87418579+NinaMina2737@users.noreply.github.com>
2023-06-11 08:03:02 +0900

    Update matrix.py

--- src/matrix.py
+++ src/matrix.py
@@ -6,7 +6,7 @@ import math
 from vector import Vector3
 
 class Matrix:
-    def __init__(self, rows=4, cols=4, data=None):
+    def __init__(self, rows=None, cols=None, data=None):
         if not rows is None and not cols is None:
             if not isinstance(rows, int) or not isinstance(cols, int):
                 raise ValueError("Matrix.__init__ takes two integers as rows and columns")
@@ -45,21 +45,45 @@ class Matrix:
         return Matrix([[self.data[i][j] - other.data[i][j] for j in range(self.cols)] for i in range(self.rows)])
 
     def __mul__(self, other):
-        if not self.cols == other.rows:
-            raise ValueError("Matrix.__mul__ takes a matrix with the same number of rows as the number of columns of this matrix")
         if isinstance(other, Matrix):
+            if not self.cols == other.rows:
+                raise ValueError("Matrix.__mul__ takes a matrix with the same number of rows as the number of columns of this matrix")
             return Matrix([[sum([self.data[i][k] * other.data[k][j] for k in range(self.cols)]) for j in range(other.cols)] for i in range(self.rows)])
         elif isinstance(other, Vector3):
-            return Vector3([sum([self.data[i][j] * other.data[j] for j in range(self.cols)]) for i in range(self.rows)])
+            matrix = [
+                other.data[0],
+                other.data[1],
+                other.data[2]
+            ]
+            if self.rows == 4:
+                matrix.append(1)
+            elif self.rows != 3:
+                raise ValueError("Matrix.__mul__ takes a 3x3 or 4x4 matrix")
+            elements = [sum([matrix[j] * self.data[i][j] for j in range(self.cols)]) for i in range(self.rows)]
+            if self.rows == 4:
+                elements = elements[:3]
+            return Vector3(elements)
         else:
             return Matrix([[self.data[i][j] * other for j in range(self.cols)] for i in range(self.rows)])
 
     def __rmul__(self, other):
-        if not self.rows == other.cols:
-            raise ValueError("Matrix.__rmul__ takes a matrix with the same number of columns as the number of rows of this matrix")
         if isinstance(other, Matrix):
+            if not self.rows == other.cols:
+                raise ValueError("Matrix.__rmul__ takes a matrix with the same number of columns as the number of rows of this matrix")
             return Matrix([[sum([other.data[i][k] * self.data[k][j] for k in range(self.cols)]) for j in range(other.cols)] for i in range(self.rows)])
         elif isinstance(other, Vector3):
-            return Vector3([sum([other.data[j] * self.data[i][j] for j in range(self.cols)]) for i in range(self.rows)])
+            data = [
+                other.data[0],
+                other.data[1],
+                other.data[2]
+            ]
+            if self.rows == 4:
+                data.append(1)
+            elif self.rows != 3:
+                raise ValueError("Matrix.__rmul__ takes a 3x3 or 4x4 matrix")
+            elements = [sum([data[j] * self.data[i][j] for j in range(self.cols)]) for i in range(self.rows)]
+            if self.rows == 4:
+                elements = elements[:3]
+            return Vector3(elements)
         else:
             return Matrix([[other * self.data[i][j] for j in range(self.cols)] for i in range(self.rows)])
