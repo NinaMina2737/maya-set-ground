434ed6b1427b2548dc0785e254696610f6c2f395
NinaMina2737 <87418579+NinaMina2737@users.noreply.github.com>
2023-06-13 21:46:13 +0900

    Update vector.py

--- src/vector.py
+++ src/vector.py
@@ -4,6 +4,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import math
 
+DIFFERENCE_THRESHOLD = 1e-6
+
 class Vector3:
     def __init__(self, elements=None):
         if not elements is None:
@@ -28,34 +30,52 @@ class Vector3:
         return len(self.elements)
 
     def __add__(self, other):
-        return Vector3([self.elements[i] + other.elements[i] for i in range(3)])
+        vector = Vector3([self.elements[i] + other.elements[i] for i in range(3)])
+        vector.make_zero()
+        return vector
 
     def __sub__(self, other):
-        return Vector3([self.elements[i] - other.elements[i] for i in range(3)])
+        vector = Vector3([self.elements[i] - other.elements[i] for i in range(3)])
+        vector.make_zero()
+        return vector
 
     def __mul__(self, other):
         if not isinstance(other, (int, float)):
             raise ValueError("Vector.__mul__ takes a scalar")
-        return Vector3([self.elements[i] * other for i in range(3)])
+        vector = Vector3([self.elements[i] * other for i in range(3)])
+        vector.make_zero()
+        return vector
 
     def __truediv__(self, other):
         if not isinstance(other, (int, float)):
             raise ValueError("Vector.__div__ takes a scalar")
-        return Vector3([self.elements[i] / other for i in range(3)])
+        vector = Vector3([self.elements[i] / other for i in range(3)])
+        vector.make_zero()
+        return vector
 
     def __abs__(self):
         return math.sqrt(self.elements[0] * self.elements[0] + self.elements[1] * self.elements[1] + self.elements[2] * self.elements[2])
 
+    def make_zero(self):
+        self.elements = [0 if abs(element) < DIFFERENCE_THRESHOLD else element for element in self.elements]
+        self.x = self.elements[0]
+        self.y = self.elements[1]
+        self.z = self.elements[2]
+
     def normalize(self):
-        return self / abs(self)
+        vector = self / abs(self)
+        vector.make_zero()
+        return vector
 
     def dot(self, other):
         return sum([self.elements[i] * other.elements[i] for i in range(3)])
 
     def cross(self, other):
-        return Vector3((self.elements[1] * other.elements[2] - self.elements[2] * other.elements[1],
+        vector = Vector3((self.elements[1] * other.elements[2] - self.elements[2] * other.elements[1],
                       self.elements[2] * other.elements[0] - self.elements[0] * other.elements[2],
                       self.elements[0] * other.elements[1] - self.elements[1] * other.elements[0]))
+        vector.make_zero()
+        return vector
 
     def norm(self):
         return abs(self)
